a9b00b8c9005b9521b9878f83302944f2ddc9107
hyj765 <hyj765@naver.com>
2022-03-10 11:24:02 +0900

    c++ basic_server and client

---
+++ Basic_Server.cpp
@@ -0,0 +1,45 @@
+﻿#pragma comment(lib, "ws2_32")
+#include<iostream>
+#include<WinSock2.h>
+#include<string>
+using namespace std;
+
+int main() {
+	WSADATA wsa;
+	WSAStartup(MAKEWORD(2, 2), &wsa);
+	SOCKET s = socket(AF_INET,SOCK_STREAM ,0);
+	SOCKADDR_IN adr;
+	SOCKADDR_IN cadr;
+	SOCKET sc;
+	adr.sin_family = AF_INET;
+	adr.sin_port = htons(80002);
+	adr.sin_addr.S_un.S_addr = INADDR_ANY; // sever의 ip주소를 자동으로 찾아서 할당해주는 명령어
+	if (0 > bind(s, (SOCKADDR*)&adr, sizeof(adr)) ) {
+		cout << "binding system error" <<endl;
+		closesocket(s);
+		WSACleanup();
+		return 0;
+	}
+	if (0 > listen(s, 5)) {
+		cout << "listen error" << endl;
+		closesocket(s);
+		WSACleanup();
+		return 0;
+	}
+	int len = sizeof(cadr);
+	sc = accept(s, (SOCKADDR*)&cadr, &len);
+	cout << cadr.sin_addr.S_un.S_addr << "가 접속했습니다." << endl;
+	while (1) {
+		string message;
+		getline(cin, message);
+		if (message == "quit") {
+			send(sc,"", 0, 0);
+			break;
+		}
+		send(sc, message.c_str(), sizeof(message), 0);
+		
+	}
+	closesocket(sc);
+	WSACleanup();
+	return 0;
+}
---
+++ Basic_client.cpp
@@ -0,0 +1,41 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#pragma comment(lib,"ws2_32")
+#include<iostream>
+#include<winsock2.h>
+#define loopback "127.0.0.1"
+using namespace std;
+
+
+int main() {
+	WSADATA wsa;
+	WSAStartup(MAKEWORD(2, 2), &wsa);
+	SOCKET cs = socket(AF_INET, SOCK_STREAM, 0);
+	if (cs == NULL) {
+		cout << "client socket create fail" << endl;
+	}
+	SOCKADDR_IN usradr;
+	usradr.sin_family = AF_INET;
+	usradr.sin_port = htons(80002);
+	usradr.sin_addr.s_addr = inet_addr(loopback);
+
+	if (connect(cs, (SOCKADDR*)&usradr, sizeof(usradr)) == SOCKET_ERROR) {
+		cout << "Connect Fail" << endl;
+		closesocket(cs);
+		WSACleanup();
+		return 0;
+	}
+	while (1) {
+		char buffer[512];
+		int f = recv(cs, buffer, 512, 0);
+		if (f == 0) {
+			break;
+		}
+		cout << f << "만큼 데이터를 입력 받음" << endl;
+		cout << "server :" << buffer << endl;
+		fflush(stdout);
+	}
+	closesocket(cs);
+	WSACleanup();
+	return 0;
+}
