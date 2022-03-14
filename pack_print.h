5d04e7374d73c732be7afc08048c04b908f5bf94
hyj765 <hyj765@naver.com>
2022-03-14 11:21:44 +0900

    packet capture code (pcaplib) in windows

---
+++ pack_print.h
@@ -0,0 +1,5 @@
+
+#pragma once
+void ethernet_print(const unsigned char* packet,int flag);
+void ip_print(const unsigned char* packet,int flag);
+void tcp_print(const unsigned char* packet,int flag);
